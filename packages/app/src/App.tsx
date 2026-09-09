a3989b5c42d340791657baff3349a0bd17e80322
Omar Soriano <osoriano2@gmail.com>
2026-09-09 04:54:52 -0700

    add proxy auth frontend

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,8 +1,26 @@
+import { ProxiedSignInPage } from '@backstage/core-components';
 import { createApp } from '@backstage/frontend-defaults';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
 
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props =>
+      <ProxiedSignInPage {...props} provider="oauth2Proxy" />,
+  },
+});
+
 export default createApp({
-  features: [catalogPlugin, navModule, homeModule],
+  features: [
+    catalogPlugin,
+    navModule,
+    homeModule,
+    createFrontendModule({
+      pluginId: 'app',
+      extensions: [signInPage],
+    }),
+  ],
 });
