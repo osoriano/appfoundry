6af8bccfdfa4c41dcb60228ebb71edd896bc69cc
Omar <osoriano2@gmail.com>
2026-09-11 09:52:40 -0700

    rm app backend (#14)

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -10,7 +10,6 @@ import { createBackend } from '@backstage/backend-defaults';
 
 const backend = createBackend();
 
-backend.add(import('@backstage/plugin-app-backend'));
 backend.add(import('@backstage/plugin-proxy-backend'));
 
 // scaffolder plugin
