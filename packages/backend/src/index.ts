7473d9ffe16280097c36f5213e188d9e53642929
Omar Soriano <osoriano2@gmail.com>
2026-09-11 15:53:28 +0000

    Revert "add proxy auth backend (#6)"
    This reverts commit 9a3b2e22c014098912d860782bb7db7194b03c9f.

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -29,11 +29,6 @@ backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
 
-// See https://backstage.io/docs/auth/oauth2-proxy/provider#backend-installation
-backend.add(
-  import('@backstage/plugin-auth-backend-module-oauth2-proxy-provider'),
-);
-
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(
