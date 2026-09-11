06fd8cb69759c9836831ae17481893c7b0256f41
Omar Soriano <osoriano2@gmail.com>
2026-09-11 15:53:20 +0000

    revert auth 1

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,28 +1,14 @@
-import { ProxiedSignInPage } from '@backstage/core-components';
 import { createApp } from '@backstage/frontend-defaults';
-import { createFrontendModule } from '@backstage/frontend-plugin-api';
-import { SignInPageBlueprint } from '@backstage/plugin-app-react';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import apiDocsModuleCrd from '@terasky/backstage-plugin-api-docs-module-crd';
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
-
-const signInPage = SignInPageBlueprint.make({
-  params: {
-    loader: async () => props =>
-      <ProxiedSignInPage {...props} provider="oauth2Proxy" />,
-  },
-});
 
 export default createApp({
   features: [
     catalogPlugin,
     navModule,
     homeModule,
-    createFrontendModule({
-      pluginId: 'app',
-      extensions: [signInPage],
-    }),
     apiDocsModuleCrd,
   ],
 });
